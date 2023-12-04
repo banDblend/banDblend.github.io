0d3e1cbc340d43f7c532e46440ba4dd159a14adf
ban <133645127+banDblend@users.noreply.github.com>
2023-12-04 19:34:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
    question.innerHTML = "Aaaaa, I like you too";
-   gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; 
+   gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+   yesBtn.style.display = "none";
+   noBtn.style.display = "none"; 
 });
 
 noBtn.addEventListener("mouseover", () => {
