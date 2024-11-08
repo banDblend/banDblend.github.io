dc1fd8bd32720f0c760849b1169af0ec42285c26
ban <133645127+banDblend@users.noreply.github.com>
2024-11-08 11:35:04 +0800

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-   question.innerHTML = "Aaaaa, I like you too";
+   question.innerHTML = "Aaaaa, I think you're pretty too";
    gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
    yesBtn.style.display = "none";
    noBtn.style.display = "none"; 
